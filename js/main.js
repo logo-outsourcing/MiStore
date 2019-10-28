5565a4e8cb6ba833af2638c0f646c37a08717ba2
vikytrusowa <vikytrusowa.@gmail.com>
2019-10-28 20:43:03 +0200

    editing page and add modile leave

--- js/main.js
+++ js/main.js
@@ -1,31 +1,37 @@
 $(document).on('click', 'a', function (event) {
     event.preventDefault();
 });
-// $(".content__call").on("click", function () {
-//     $("#request ").toggleClass("content__visible");
-// });
-// $(".content__modal--link").on("click", function () {
-//     $("#request ").addClass("content__visible");
-// });
-// $(".js__thank").on("click", function () {
-//     $("#thank ").removeClass("content__visible");
-//     $("#request ").addClass("content__visible");
-// });
-//
-// $(".js__close").on("click", function () {
-//     $("#request ").addClass("content__visible");
-// });
 jQuery(document).ready(function ($) {
     $('.pop').magnificPopup({
         type: 'inline',
         mainClass: 'my-mfp-zoom-in',
     });
 });
+
+$(".thank__button").on("click",function () {
+    var magnificPopup = $.magnificPopup.instance;
+    magnificPopup.close();
+});
+$(document).mouseleave(function(e){
+    if (e.clientY < 10) {
+        $('.content').magnificPopup({
+            items: {
+                src: '#leaves',
+                type: 'inline'
+            }
+        }).magnificPopup('open');
+    }
+});
+$(document).click(function(e) {
+    if (($("#leaves").is(':visible')) && (!$(e.target).closest("#leaves .content__box").length)) {
+        $("#leaves").remove();
+    }
+});
 $(".js__close").on("click",function () {
     var magnificPopup = $.magnificPopup.instance;
     magnificPopup.close();
 });
-$(".thank__button").on("click",function () {
+$(".js-shut").on("click",function () {
     var magnificPopup = $.magnificPopup.instance;
     magnificPopup.close();
 });
